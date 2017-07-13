678cfa6056b218f918bf90f015bf6cf6429f4539
samr28 <samroq@yahoo.com>
2017-07-13 20:54:32 +0000

    style(js/cookie.js) add brackets for all functions

--- js/cookie.js
+++ js/cookie.js
@@ -1,30 +1,33 @@
 function createCookie(name, value, expires, path, domain) {
   var cookie = name + "=" + escape(value) + ";";
 
-  if (expires) {
-    // If it's a date
-    if(expires instanceof Date) {
-      // If it isn't a valid date
-      if (isNaN(expires.getTime()))
-       expires = new Date();
+    if (expires) {
+        // If it's a date
+        if(expires instanceof Date) {
+          // If it isn't a valid date
+            if (isNaN(expires.getTime())) {
+                expires = new Date();
+            }
+        }
+        else {
+            expires = new Date(new Date().getTime() + parseInt(expires) * 1000 * 60 * 60 * 24);
+        }
+        cookie += "expires=" + expires.toGMTString() + ";";
     }
-    else
-      expires = new Date(new Date().getTime() + parseInt(expires) * 1000 * 60 * 60 * 24);
 
-    cookie += "expires=" + expires.toGMTString() + ";";
-  }
-
-  if (path)
-    cookie += "path=" + path + ";";
-  if (domain)
-    cookie += "domain=" + domain + ";";
-
+    if (path) {
+        cookie += "path=" + path + ";";
+    }
+    if (domain) {
+        cookie += "domain=" + domain + ";";
+    }
+    
   document.cookie = cookie;
 }
 
 function getCookie(name) {
-  name = " " + name;
-  var regexp = new RegExp("(?:^" + name + "|;\s*"+ name + ")=(.*?)(?:;|$)", "g");
-  var result = regexp.exec(document.cookie);
-  return (result === null) ? null : result[1];
+    name = " " + name;
+    var regexp = new RegExp("(?:^" + name + "|;\s*"+ name + ")=(.*?)(?:;|$)", "g");
+    var result = regexp.exec(document.cookie);
+    return (result === null) ? null : result[1];
 }
