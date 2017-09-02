4d72fc08eeeb07d599e279f5fc5ea2fc40e14678
samr28 <samroq@yahoo.com>
2017-09-02 00:52:54 -0500

    Updated external URL

--- php/const.php
+++ php/const.php
@@ -3,15 +3,15 @@
     $GLOBALS['codename'] = "Slipstream";
     $version = "4.0.1";        //Version number
     $GLOBALS['version'] = "4.0.1";
-    $externalURL = "https://sandman-scouting-kir13y.c9users.io";
-    $GLOBALS['externalURL'] = "https://sandman-scouting-kir13y.c9users.io";
+    $externalURL = "https://samr.mynetgear.com";
+    $GLOBALS['externalURL'] = "https://samr.mynetgear.com";
     $GLOBALS['OAUTH']['URI'] = "/oauthAut.php";
     $title = "2017 Scouting";
     $red = "Red";
     $blue = "Blue";
-    
+
     $GLOBALS['scoutingTeams'] = array(1259, 1306, 2077, 5148, 5976, 6574);
-    
+
     //Paths and text
     $GLOBALS['path']['index'] = "index";
     $GLOBALS['path']['settings'] = "settings";
@@ -27,7 +27,7 @@
     $GLOBALS['text']['reportSheet'] = "Game Report";
     $GLOBALS['path']['databaseSheet'] = "databasesheet";
     $GLOBALS['text']['databaseSheet'] = "DB Management";
-    
+
     $GLOBALS['DB']['serverName'] = "localhost";
     $GLOBALS['DB']['user'] = "data";
     $GLOBALS['DB']['password'] = "mxwZsnKw5FtD8uX8";
@@ -36,56 +36,56 @@
     $DBuser = "data";
     $DBpassword = "mxwZsnKw5FtD8uX8";
     $DBname = "sandman";
-    
+
     $GLOBALS['DB']['table']['matchscouting'] = "`matchdata`";
     $matchScoutingTable = "matchdata";
     $achievementTable = "`userachievements`";
     $userTable = "`users`";
     $achievementListSQL = "(`id`, `userid`, `username`, `match1`)";
-    
+
 
     $reportChannel = "#slipstream_data";
     $botName = "Mr. Sandman";
 
-    
+
     $numTeamsPerMatch = 6;                  //Number of teams in each match
-    
+
     $noUsernameName = "NoLogin";
-    
+
     //TBA CONSTANTS
     $GLOBALS['TBA']['enable'] = true;
-    $GLOBALS['TBA']['header'] = 'X-TBA-App-Id: frc1306:scouting:v' . $version;
+    $GLOBALS['TBA']['header'] = 'X-TBA-App-Id: frc1306:scouting:v' . $GLOBALS['version'];
     $GLOBALS['TBA']['eventCode'] = 'wila';          //Wisc = wimi, LaCrosse = wila
     $GLOBALS['TBA']['year'] = 2017;
     $GLOBALS['TBA']['matchType'] = 'qm';        //qm        f=finals, qf=quarterfinal, qm=qual, sf=semifinal
     $GLOBALS['TBA']['cache']['teams'] = 'cache.json';
     $GLOBALS['TBA']['cache']['matches'] = 'cacheMatch.json';
     $GLOBALS['TBA']['cache']['update'] = 'cacheUpdate.json';
-    
+
     //SLACK BOT CONSTANTS
     $GLOBALS['SlackBot']['enable'] = false;
     $GLOBALS['SlackBot']['slackhook'] = 'https://hooks.slack.com/services/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';
     $GLOBALS['SlackBot']['username'] = 'Mr. Sandman';
     $GLOBALS['SlackBot']['reportchannel'] = '#slipstream_data';
     $GLOBALS['SlackBot']['notifyusers'] = '';
-    
-    
+
+
     $GLOBALS['TBAToSlack']['username'] = 'Announcer McAnnouncerpants';
     $GLOBALS['TBAToSlack']['channel'] = '#testing';
     $GLOBALS['TBAToSlack']['icon'] = ':netscape:';
     $GLOBALS['TBAToSlack']['frcteam'] = 118;
     $GLOBALS['TBAToSlack']['cache'] = 'tbaToSlack.json';
-    
-    
+
+
     //ACHIEVEMENT CONSTANTS
     $achievementEnable = false;
     $GLOBALS['enable']['achievements'] = false;
     $GLOBALS['achievements']['startCol'] = 4;
     $GLOBALS['achievements']['name'] = array("Thats how you do it");
     $GLOBALS['achievements']['desc'] = array("Submit your first sheet");
-    
+
     $achievementStartCol = 4;
-    
+
     $achievementNumMatch1 = 1;
     $achievementName = array("Thats how you do it");
     $achievementDesc = array("Submit your first sheet");
