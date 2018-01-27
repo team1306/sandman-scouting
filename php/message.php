847d96aeda84c2657c7ea015be031a847ab8f842
samr28 <samroq@yahoo.com>
2018-01-27 15:20:29 -0600

    Make alerts dismissable and add error reporting

--- php/message.php
+++ php/message.php
@@ -6,6 +6,10 @@
     function sendMessage($message, $path) {
       $_SESSION['message'] = $message;
       // echo var_dump($message);
+      if (headers_sent()) {
+        die("Redirect failed. Please notify an admin on Slack.
+        For now, click <a href='{$path}'>this link</a>.");
+      }
       header('Location: ' . $path);
     }
   }
@@ -15,12 +19,16 @@
   if (!function_exists('checkMessage')) {
     function checkMessage() {
       if (isset($_SESSION['message'])) {
-        echo '<br>
-            <div class="container">
-                <div class="alert alert-' . $_SESSION['message']['type'] . '">
-                    <strong>' . $_SESSION['message']['name'] . '</strong> ' . $_SESSION['message']['desc'] . '
-                </div>
-            </div>';
+        echo "
+            <br>
+            <div class='container'>
+              <div class='alert alert-dismissible alert-{$_SESSION['message']['type']}' role='alert'>
+                <strong>{$_SESSION['message']['name']}</strong> {$_SESSION['message']['desc']}
+                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                  <span aria-hidden='true'>&times;</span>
+                </button>
+              </div>
+            </div>";
         unset($_SESSION['message']);
       }
     }
