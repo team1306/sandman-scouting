f011208815a4a52bbb59a2294c7adbd0facb472c
Sam Roquitte <samroq@yahoo.com>
2019-03-15 22:48:53 -0400

    function_exists checking and docs

--- getMatchNum.php
+++ getMatchNum.php
@@ -1,12 +1,18 @@
 <?php
-    function getLastMatch() {
-        include 'php/dbDataConn.php';
-        $recentMatchSQL = mysqli_query($dbDataConn, "SELECT `matchNum` FROM `matchdata` ORDER BY `id` DESC limit 1");
-        while($row = mysqli_fetch_array($recentMatchSQL)) {
-            return $row['matchNum'];
+    // Gets the last match number that was submitted to the DB
+    if (!function_exists("getLastMatch")) {
+        function getLastMatch() {
+            include 'php/dbDataConn.php';
+            $recentMatchSQL = mysqli_query($dbDataConn, "SELECT `matchNum` FROM `matchdata` ORDER BY `id` DESC limit 1");
+            while($row = mysqli_fetch_array($recentMatchSQL)) {
+                return $row['matchNum'];
+            }
         }
     }
-    function getNextMatch() {
-        return getLastMatch()+1;
+    // Gets the next match number (last match + 1)
+    if (!function_exists("getNextMatch")) {
+        function getNextMatch() {
+            return getLastMatch()+1;
+        }
     }
 ?>
