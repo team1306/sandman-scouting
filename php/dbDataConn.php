24a1de70831b9146295c0f33b99f9929dd5c504c
Sam Roquitte <samroq@yahoo.com>
2019-03-15 22:53:36 -0400

    Docs/style

--- php/dbDataConn.php
+++ php/dbDataConn.php
@@ -1,7 +1,14 @@
 <?php
-include 'const.php';
-$dbDataConn = mysqli_connect($GLOBALS['DB']['HOST'],$GLOBALS['DB']['USER'],$GLOBALS['DB']['PW'],$GLOBALS['DB']['DATABASE']);
-if ($dbDataConn->connect_error || $dbDataConn === false) {
-    die("Database Connection failed: " . $dbDataConn->connect_error);
-}
+
+    // This script inits a connection to the DB
+    // It should be included in other PHP files
+
+    include 'const.php';
+
+    $dbDataConn = mysqli_connect($GLOBALS['DB']['HOST'],$GLOBALS['DB']['USER'],$GLOBALS['DB']['PW'],$GLOBALS['DB']['DATABASE']);
+
+    if ($dbDataConn->connect_error || $dbDataConn === false) {
+        die("Database Connection failed: " . $dbDataConn->connect_error);
+    }
+
 ?>
