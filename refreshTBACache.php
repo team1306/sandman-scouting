498531629ba95d5ace4c2c5564867c80d4480b3a
Sam Roquitte <samroq@yahoo.com>
2019-03-25 11:09:18 -0400

    TBACache debug

--- refreshTBACache.php
+++ refreshTBACache.php
@@ -6,5 +6,7 @@ $_SESSION['error']['name'] = "TBA Cache Updated!";
 $_SESSION['error']['desc'] = "Last Update: " . $TBAdata->getUpdateTime();
 $_SESSION['error']['type'] = 1;
 file_put_contents("TBAcacheUpdate.log", file_get_contents("TBAcacheUpdate.log") . $TBAdata->getUpdateTime() . "\r\n");
-echo '<script>window.location.replace("../admin.php");</script>';
+if (!$GLOBALS['TBA']['debug']['match'] && !$GLOBALS['TBA']['debug']['all']) {
+    echo '<script>window.location.replace("../admin.php");</script>';
+}
 ?>
