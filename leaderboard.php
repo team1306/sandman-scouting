cf2f524e2b99a8f7dd0c929e545a69c503863921
Mateo Silver <mateonsilver@gmail.com>
2019-03-26 17:08:30 -0400

    echoing to compare values 4

--- leaderboard.php
+++ leaderboard.php
@@ -49,9 +49,10 @@
         $result = mysqli_query($dbDataConn, "SELECT * FROM " . $GLOBALS['DB']['TABLE']['USER'] . " ORDER BY matchesScouted DESC");
         while($row = mysqli_fetch_array($result)) {
             $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['uid']=$row['id'];
-                $tableName = echo $row['name'];
-                $slackName = echo $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name'];
-            if($tableName == $slackName){
+                echo $row['name'];
+                echo $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name'];
+                echo $row['name'] == $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name'];
+            if($row['name'] == $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name']){
                 echo '<tr class="bg-danger">';
                   echo "<td>" . $row['team'] . "</td>";
                   echo "<td>" . $row['name'] . "</td>";
