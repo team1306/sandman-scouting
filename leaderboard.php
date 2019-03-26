e49c42b03df71ee72a487ccc2cc7459de9fa20fc
Mateo Silver <mateonsilver@gmail.com>
2019-03-26 15:56:43 -0400

    testing colors

--- leaderboard.php
+++ leaderboard.php
@@ -49,8 +49,10 @@
 
         while($row = mysqli_fetch_array($result)) {
             $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['uid']=$row['id'];
+                echo $row['name'];
+                echo $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name'];
             if($row['name'] == $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name']){
-                echo "<b><tr>";
+                echo "<tr class="bg-danger">";
                   echo "<td>" . $row['team'] . "</td>";
                   echo "<td>" . $row['name'] . "</td>";
                   echo "<td>" . $row['slackId'] . "</td>";
