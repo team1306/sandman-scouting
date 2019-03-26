f32a1a91ea21d48f9b4e6d790d6b4828409c1484
Mateo Silver <mateonsilver@gmail.com>
2019-03-25 23:38:07 -0400

    Display user id on settings page 13

--- settings.php
+++ settings.php
@@ -26,7 +26,7 @@
         </div>
         <br>
         <p>
-            Your Used ID is: <?php echo $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['slackId']?>
+            Your Used ID is: <?php echo $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['slackId'];?>
         </p>
       <form action="settingSubmit" method="post">
         <?php
