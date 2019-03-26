910ccd6e370bee2bfff969601b817a80f5dfdcd3
Mateo Silver <mateonsilver@gmail.com>
2019-03-26 17:24:04 -0400

    echoing to compare values 11

--- leaderboard.php
+++ leaderboard.php
@@ -55,22 +55,28 @@
                 echo $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name'];
                 echo "\r\nif statement";
                 echo $row['name'] == $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name'];
-            if(echo ($row['name'] == $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name']) == 1){
-                echo '<tr class="bg-danger">';
-                  echo "<td>" . $row['team'] . "</td>";
-                  echo "<td>" . $row['name'] . "</td>";
-                  echo "<td>" . $row['slackId'] . "</td>";
-                  echo "<td>" . $row['matchesScouted'] . "</td>";
-                echo "</tr>";
-            }
-            else if(echo ($row['name'] == $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name']) != 1){
-            echo "<tr>";
-              echo "<td>" . $row['team'] . "</td>";
-              echo "<td>" . $row['name'] . "</td>";
-              echo "<td>" . $row['slackId'] . "</td>";
-              echo "<td>" . $row['matchesScouted'] . "</td>";
-            echo "</tr>";
-            }
+
+            <script>
+                	if (<?php echo ($row['name'] == $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name']); ?> == 1) {
+                        <?php
+                        echo '<tr class="bg-danger">';
+                          echo "<td>" . $row['team'] . "</td>";
+                          echo "<td>" . $row['name'] . "</td>";
+                          echo "<td>" . $row['slackId'] . "</td>";
+                          echo "<td>" . $row['matchesScouted'] . "</td>";
+                        echo "</tr>";
+                        ?>
+                	} else {
+                        <?php
+                        echo '<tr>';
+                          echo "<td>" . $row['team'] . "</td>";
+                          echo "<td>" . $row['name'] . "</td>";
+                          echo "<td>" . $row['slackId'] . "</td>";
+                          echo "<td>" . $row['matchesScouted'] . "</td>";
+                        echo "</tr>";
+                        ?>
+                	}
+            </script>
         }
         echo "</tbody></table></div>";
 
