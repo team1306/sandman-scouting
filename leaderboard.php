e4e5c77fb04578a1b9fd442ee66bc291f3d8fa2d
Mateo Silver <mateonsilver@gmail.com>
2019-03-26 16:25:35 -0400

    echoing

--- leaderboard.php
+++ leaderboard.php
@@ -50,8 +50,8 @@
 
         while($row = mysqli_fetch_array($result)) {
             $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['uid']=$row['id'];
-                echo $row['name'];
-                echo $_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name'];
+                echo REPLACE(LOWER($row['name']), ' ', '');
+                echo REPLACE(LOWER($_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name']), ' ', '');
             if(REPLACE(LOWER($row['name']), ' ', '') == REPLACE(LOWER($_SESSION[$GLOBALS['APP_INFO']['SHORT_NAME']]['userArray']['name']), ' ', '')){
                 echo '<tr class="bg-danger">';
                   echo "<td>" . $row['team'] . "</td>";
